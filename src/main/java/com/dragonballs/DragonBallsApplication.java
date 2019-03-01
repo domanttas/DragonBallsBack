2f5bbc5c6f29b59e3960c2ff7a237a6f5051aa4b
domanttas <domantasgiedraitis@gmail.com>
2019-03-01 16:10:34 +0200

    Initial commit

---
+++ src/main/java/com/dragonballs/DragonBallsApplication.java
@@ -0,0 +1,13 @@
+package com.dragonballs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonBallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonBallsApplication.class, args);
+	}
+
+}
