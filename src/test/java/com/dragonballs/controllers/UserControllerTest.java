87ae42d9b2d7003fafd73602b3062c8865747a60
eparis@atlassian.com <raimundas.maciulevicius@stud.vgtu.lt>
2019-03-06 15:09:15 +0200

    feat(ITA3): Added unit tests for registration endpoints

---
+++ src/test/java/com/dragonballs/controllers/UserControllerTest.java
@@ -0,0 +1,60 @@
+package com.dragonballs.controllers;
+
+import com.dragonballs.entities.User;
+import com.dragonballs.responsedatamapping.UserLocationCreator;
+import com.dragonballs.services.user.UserService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import javax.jws.soap.SOAPBinding;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserControllerTest {
+
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private UserLocationCreator userLocationCreator;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void createResponse_ifUserProvided(){
+        User user = new User();
+        Mockito.when(userService.registerUser(user)).thenReturn(user);
+
+        URI location = URI.create("location-endpoint");
+        Mockito.when(userLocationCreator.userLocationCreator(user)).thenReturn(location);
+
+        ResponseEntity responseEntity = userController.registerUser(user);
+
+        Assert.assertEquals("[location-endpoint]", responseEntity.getHeaders().get("location").toString());
+    }
+
+    @Test
+    public void createResponse_fetchUsers(){
+        User user = new User();
+        Mockito.when(userService.registerUser(user)).thenReturn(user);
+
+        ResponseEntity responseEntity = userController.registerUser(user);
+
+        boolean isListEmpty = responseEntity.hasBody();
+
+        Assert.assertEquals(false, isListEmpty);
+    }
+}
--- src/test/java/com/dragonballs/tests/UserServiceImplementationTests.java
+++ src/test/java/com/dragonballs/tests/UserServiceImplementationTests.java
@@ -2,4 +2,6 @@ package com.dragonballs.tests;
 
 public class UserServiceImplementationTests {
 
+
+
 }
