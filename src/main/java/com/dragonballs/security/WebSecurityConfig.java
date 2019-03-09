a7b64ef440a31d6cc066229a1b21051f92748255
domanttas <domantasgiedraitis@gmail.com>
2019-03-09 18:53:04 +0200

    feat(ITA3): Added CORS mapping

--- src/main/java/com/dragonballs/security/WebSecurityConfig.java
+++ src/main/java/com/dragonballs/security/WebSecurityConfig.java
@@ -2,16 +2,19 @@ package com.dragonballs.security;
 
 import com.dragonballs.services.authentication.JwtAuthorizationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Autowired
     JwtAuthorizationTokenFilter authenticationTokenFilter;
@@ -39,5 +42,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers()
                 .frameOptions().sameOrigin()  // required to set for H2 else H2 Console will be blank.
                 .cacheControl();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**");
     }
 }
