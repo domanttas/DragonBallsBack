e37178c4e663df65537f5c0466be02a41bd4b034
domanttas <domantasgiedraitis@gmail.com>
2019-03-20 17:35:26 +0200

    feat(ITA3): Blog validation bug fix

--- src/main/java/com/dragonballs/services/blog/BlogValidator.java
+++ src/main/java/com/dragonballs/services/blog/BlogValidator.java
@@ -1,7 +1,10 @@
 package com.dragonballs.services.blog;
 
+import com.dragonballs.dao.UserDAO;
 import com.dragonballs.entities.Blog;
+import com.dragonballs.entities.User;
 import com.dragonballs.exceptions.BlogException;
+import com.dragonballs.exceptions.UserException;
 import com.dragonballs.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,11 +14,11 @@ import org.springframework.stereotype.Service;
 public class BlogValidator {
 
     @Autowired
-    private UserService userService;
+    private UserDAO userDAO;
 
     private static final String ALL_FIELD_ARE_REQUIRED = "All field are required";
 
-    public boolean validate (Blog blog) {
+    public boolean validate(Blog blog) {
 
         if (blog.getBlogText().isEmpty()) {
             throw new BlogException(ALL_FIELD_ARE_REQUIRED);
@@ -23,7 +26,16 @@ public class BlogValidator {
             throw new BlogException(ALL_FIELD_ARE_REQUIRED);
         }
 
-        userService.validateUser(blog.getUser());
+        validateUser(blog.getUser());
+
         return true;
+    }
+
+    private void validateUser(User user) {
+        User existingUser = userDAO.findByUsername(user.getUsername());
+
+        if (existingUser == null) {
+            throw new UserException("User does not exist");
+        }
     }
 }
